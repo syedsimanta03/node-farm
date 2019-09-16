309830f64938759cc27ff0421b63cfa80bde1fe6
syedsimanta03 <syed.simanta06@gmail.com>
2019-09-16 19:15:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,11 @@ const url = require('url');
 
 var static = require('node-static');
 var file = new static.Server();
-require('http')
-	.createServer(function(request, response) {
-		request
-			.addListener('end', function() {
-				file.serve(request, response);
-			})
-			.resume();
-	})
-	.listen(process.env.PORT || 8000);
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
 /**
 |--------------------------------------------------
 | FILES
